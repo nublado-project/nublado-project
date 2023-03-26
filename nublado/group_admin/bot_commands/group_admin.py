3e4c669838dd7cdfb090afd6683c2e472818c318
nublado <christopher.nublado@gmail.com>
2023-03-26 15:31:53 -0700

    ...

--- nublado/group_admin/bot_commands/group_admin.py
+++ nublado/group_admin/bot_commands/group_admin.py
@@ -36,7 +36,7 @@ BOT_MESSAGES = {
     ),
     'welcome_agreed': _(
         "Welcome to the group, {name}.\n\n" \
-        "*You need to introduce yourself with a voice message.*\n\n" \
+        "*YOU NEED TO INTRODUCE YOURSELF WITH A VOICE MESSAGE.*\n\n" \
         "This is our our protocol for new members. It helps us filter out fake accounts, trolls, etc.\n\n" \
         "We look forward to hearing from you."
     ),
