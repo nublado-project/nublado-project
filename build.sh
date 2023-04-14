fb25119a7100478d8080a98435cf7d14a8cacb49
Christopher <christopher.nubladao@gmail.com>
2023-04-14 14:31:11 -0700

    ...

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --no-input
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
