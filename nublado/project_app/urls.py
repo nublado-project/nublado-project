ff2dd9cb64f1affbbc6f44403eeb71eae60da3ea
nublado <christopher.nublado@gmail.com>
2023-05-19 10:41:37 -0700

    Added favicon.ico path.

--- nublado/project_app/urls.py
+++ nublado/project_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from .views import (
     HomeView,
@@ -9,5 +11,6 @@ app_name = 'project_app'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('health', HealthView.as_view(), name='health')
+    path('health', HealthView.as_view(), name='health'),
+	path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
 ]
