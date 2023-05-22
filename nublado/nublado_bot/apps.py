d7e5ecdb0cc502e96aae58622df7ca464f4b22d4
Christopher <christopher@nublado.com>
2023-05-22 06:27:05 -0700

    Fixed group points.

--- nublado/nublado_bot/apps.py
+++ nublado/nublado_bot/apps.py
@@ -15,7 +15,10 @@ class NubladoBotConfig(AppConfig):
 
     def ready(self):
         from .bot_commands.group_points import (
-            add_points_handler, remove_points_handler
+            add_point_handler,
+            add_points_handler,
+            remove_point_handler,
+            remove_points_handler
         )
         from .bot_commands.group_notes import (
             group_notes,
@@ -24,8 +27,6 @@ class NubladoBotConfig(AppConfig):
             get_group_note_handler
         )
         from .bot_commands.group_admin import(
-            # update_group_admins,
-            # get_non_members,
             member_join_handler,
             member_exit_handler,
             welcome_button_handler,
@@ -42,12 +43,10 @@ class NubladoBotConfig(AppConfig):
         # Register handlers
         
         # group_admin
-        # bot.add_command_handler('update_group_admins', update_group_admins)
-        # bot.add_command_handler('get_non_members', get_non_members)
         bot.add_command_handler('set_bot_language', set_bot_language)
-        bot.add_handler(member_join_handler, handler_group=2)
-        bot.add_handler(member_exit_handler, handler_group=2)
-        bot.add_handler(welcome_button_handler, handler_group=2)
+        bot.add_handler(member_join_handler)
+        bot.add_handler(member_exit_handler)
+        bot.add_handler(welcome_button_handler)
         # misc
         bot.add_command_handler('start', start)
         bot.add_command_handler('reverse', reverse_text)
@@ -57,12 +56,14 @@ class NubladoBotConfig(AppConfig):
         bot.add_command_handler('roll_sum', roll_sum)
         bot.add_command_handler('get_time', get_time)
         # group_points
-        bot.add_handler(add_points_handler, handler_group=2)
-        bot.add_handler(remove_points_handler, handler_group=2)
+        bot.add_handler(add_point_handler)
+        bot.add_handler(add_points_handler)
+        bot.add_handler(remove_point_handler)
+        bot.add_handler(remove_points_handler)
         # notes
         bot.add_command_handler('group_notes', group_notes)
         bot.add_command_handler('save_group_note', save_group_note)
         bot.add_command_handler('remove_group_note', remove_group_note)
-        bot.add_handler(get_group_note_handler, handler_group=2)
+        bot.add_handler(get_group_note_handler)
         # Add the bot to the registry.
         bot_registry.add_bot(NubladoBotConfig.bot_key, bot)
