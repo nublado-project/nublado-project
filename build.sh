01bedfffe1b7ae3f3aa44613633e10ca22afbc52
Christopher <christopher@nublado.com>
2023-08-16 12:09:51 -0700

    ...

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py makemigrations
+python manage.py makemigrations django_telegram users
 python manage.py migrate
 python manage.py collectstatic --no-input
 
