4c72223e07d85db2ebc6bfecffecad22f0af047f
Christopher <christopher@nublado.com>
2023-08-16 12:42:59 -0700

    shit

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
+python manage.py migrate django_telegram users
 python manage.py collectstatic --no-input
 
 if [[ $CREATE_SUPERUSER = true ]];
