1c184d491bdf07d9f8eee41819e2a9748a20d67c
Christopher <christopher@nublado.com>
2023-08-16 13:07:44 -0700

    ...

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 
 pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py makemigrations
+python manage.py findstatic --verbosity 2 favicon.ico
 python manage.py migrate
-python manage.py migrate django_telegram users
 python manage.py collectstatic --no-input
 
 if [[ $CREATE_SUPERUSER = true ]];
