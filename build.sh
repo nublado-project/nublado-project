3f8423c7cf837a8a904f822528553827a82f9b5e
Christopher <christopher@nublado.com>
2023-08-16 11:54:16 -0700

    Pancakes

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 
 pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py makemigrations users
 python manage.py migrate
-python manage.py migrate users
 python manage.py collectstatic --no-input
 
 if [[ $CREATE_SUPERUSER = true ]];
