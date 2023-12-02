b287e137245e32814c9d3411093ce982efb9e5ac
Christopher <christopher@nublado.com>
2023-12-02 13:21:29 -0800

    Pancakes

--- nublado/bot_misc/bot_messages.py
+++ nublado/bot_misc/bot_messages.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext as _
 
 BOT_MESSAGES = {
     'dice_roll': _("bot.message.dice_roll {member} {dice}"),
-    'dice_roll_total': _("bot.message.dice_roll_total{member} {dice} {total}"),
+    'dice_roll_total': _("bot.message.dice_roll_total {member} {dice} {total}"),
     'dice_specify_num': _("bot.message.dice_specify_number {min_dice} {max_dice}"),
     'get_time': _("It's {weekday}, {time} {timezone}."),
     'start_bot': _("Hello, {member}. {bot_name} has started."),
