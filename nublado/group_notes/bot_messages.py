6d256bdf0d460218569408e27f7ff238b885b3a0
Christopher <christopher@nublado.com>
2023-12-02 18:40:48 -0800

    ...

--- nublado/group_notes/bot_messages.py
+++ nublado/group_notes/bot_messages.py
@@ -1,15 +1,13 @@
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 BOT_MESSAGES = {
-    'no_notes': _("There are no saved notes."),
-    'notes_list': _("*Notes*\n{notes}"),
-    'note_saved': _("The note *{note_tag}* has been saved."),
-    'note_removed': _("The note *{note_tag}* has been removed."),
-    'note_no_exist': _("The note *{note_tag}* doesn't exist."),
-    'note_no_content': _("A note needs content."),
-    'note_no_tag': _("A note must have a tag."),
-    'note_no_args': _("A note needs a tag and content."),
-    'note_not_in_repo': _(
-        "The content for the note *{note_tag}* was not found in the repo."
-    )
+    'no_notes': _("bot.message.no_notes"),
+    'notes_list': _("bot.message.notes_list {notes}"),
+    'note_saved': _("bot.message.note_saved {note_tag}"),
+    'note_removed': _("bot.message.note_removed {note_tag}"),
+    'note_no_exist': _("bot.message.note_no_exist {note_tag}"),
+    'note_no_content': _("bot.message.note_no_content"),
+    'note_no_tag': _("bot.message.note_no_tag"),
+    'note_no_args': _("bot.message.note_no_args"),
+    'note_not_in_repo': _("bot.message.note_not_in_repo {note_tag}")
 }
