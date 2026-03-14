bd0e44066efac577d40ab770fef398ff138a6b4a
nublado <nublado@nublado.com>
2026-03-14 08:48:51 -0700

    ...

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-gunicorn config.asgi:application -w 4 -k uvicorn.workers.UvicornWorker
+gunicorn config.asgi:application -w 1 -k uvicorn.workers.UvicornWorker
 python manage.py set_bot_webhooks
 
