74582a497f0865a264331121007909d19b68aaaf
nublado <nublado@nublado.com>
2026-03-14 00:11:51 -0700

    ...

--- nublado/apps/nublado_bot/handlers/misc.py
+++ nublado/apps/nublado_bot/handlers/misc.py
@@ -1,9 +1,10 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from django.utils.translation import gettext_lazy as _
+
 from django_telegram.utils.helpers import safe_reply
 from django_telegram.utils.decorators import with_language
-from django_telegram.utils.jobs import delete_message_job
 from ..bot_messages import BOT_MESSAGES
 
 
@@ -27,16 +28,4 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=tg_chat.id,
         text=str(BOT_MESSAGES["bot_hello"]),
         reply_to_message_id=command_message.message_id
-    )
-
-    context.job_queue.run_once(
-        delete_message_job,
-        10,
-        data={
-            "chat_id": tg_chat.id,
-            "message_ids": [
-                command_message.message_id,
-                hello_message.message_id
-            ],
-        }
     )
